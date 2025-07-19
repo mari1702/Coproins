cdddbc2d2ec43adad546a7f78aee27e4f28b6aae
mari1702 <marianasantiago708@gmail.com>
2025-07-18 19:31:57 -0600

    Se corrigio vista de detalles

--- components/organisms/productDetails.php
+++ components/organisms/productDetails.php
@@ -5,30 +5,27 @@ function productDetails($producto)
     <div class="row">
 
         <?php if ($producto->getImgRuta()): ?>
-            <div class="col-md-6 col-sm-12 mb-3">
+            <div class="col-4 mb-3">
                 <img src="../<?= $producto->getImgRuta(); ?>"
-                    class="img-fluid rounded" alt="Producto 1">
+                    class="img-fluid rounded" alt="imagen">
             </div>
         <?php endif ?>
 
-        <div class="<?= ($producto->getImgRuta()) ? 'col-md-6 col-sm-12' : 'col-12' ?>">
+        <div class="<?= ($producto->getImgRuta()) ? 'col-8' : 'col-12' ?>">
             <h4 class="mb-3"><?= $producto->getDescripcion(); ?></h4>
 
             <ul class="list-group list-group-flush mb-4">
                 <li class="list-group-item">
-                    <strong>Unidad de medida:</strong> <?= $producto->getUnidadMedida()->getNombre(); ?>
-                </li>
-                <li class="list-group-item">
-                    <strong>Categoría:</strong> <?= $producto->getCategoria()->getNombre(); ?>
-                </li>
-                <li class="list-group-item">
-                    <strong>Precio:</strong> <span
-                        class="price-format"><?= $producto->getPrecio(); ?></span>
-                </li>
-                <li class="list-group-item">
-                    <strong>ID:</strong> <?= $producto->getId(); ?>
+                    <strong>Precio:</strong> 
+                    <span class="price-format">
+                        <?= $producto->getPrecio(); ?>
+                    </span>
                 </li>
             </ul>
+
+            <span class="badge bg-primary" id="id"><strong><?= $producto->getId() ?></strong></span>
+            <span class="badge bg-secondary" id="department-name"><?= $producto->getCategoria()->getNombre() ?></span>
+            <span class="badge bg-success" id="brand-name"><?= $producto->getUnidadMedida()->getNombre() ?></span>
         </div>
     </div>
 <?php
--- components/organisms/toolDetails.php
+++ components/organisms/toolDetails.php
@@ -5,27 +5,18 @@ function toolDetails($herramienta)
     <div class="row">
 
         <?php if ($herramienta->getImgRuta()): ?>
-            <div class="col-md-6 col-sm-12 mb-3">
+            <div class="col-4 mb-3">
                 <img src="../<?= $herramienta->getImgRuta(); ?>"
                     class="img-fluid rounded" alt="Producto 1">
             </div>
         <?php endif ?>
 
-        <div class="<?= ($herramienta->getImgRuta()) ? 'col-md-6 col-sm-12' : 'col-12' ?>">
+        <div class="<?= ($herramienta->getImgRuta()) ? 'col-8' : 'col-12' ?>">
             <h4 class="mb-3"><?= $herramienta->getDescripcion(); ?></h4>
 
-            <ul class="list-group list-group-flush mb-4">
-                <li class="list-group-item">
-                    <strong>Departamento:</strong> <?= $herramienta->getDepartamento()->getNombre(); ?>
-                </li>
-                <li class="list-group-item">
-                    <strong>Precio:</strong> <span
-                        class="price-format"><?= $herramienta->getMarca()->getNombre(); ?></span>
-                </li>
-                <li class="list-group-item">
-                    <strong>ID:</strong> <?= $herramienta->getId(); ?>
-                </li>
-            </ul>
+            <span class="badge bg-primary" id="id"><strong><?= $herramienta->getId() ?></strong></span>
+            <span class="badge bg-secondary" id="department-name"><?= $herramienta->getDepartamento()->getNombre() ?></span>
+            <span class="badge bg-success" id="brand-name"><?= $herramienta->getMarca()->getNombre() ?></span>
         </div>
     </div>
 
