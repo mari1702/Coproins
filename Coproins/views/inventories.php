54e3f0dfaa6f9aba25e5242aacd8260197b78ce8
mari1702 <marianasantiago708@gmail.com>
2025-07-22 02:04:23 -0600

    Se corrigio visualizacion de tarjetas

--- Coproins/views/inventories.php
+++ Coproins/views/inventories.php
@@ -381,21 +381,23 @@ startTemplate("Inventarios");
                         <div class="card shadow-sm mb-4 border-0">
                             <div class="card-body p-3">
                                 <div class="row align-items-center g-3 mb-3">
+
                                     ${item.img == null ? " " : `    
-                                    <div class="col-2 col-sm-2 col-md-2 col-lg-2">
+                                    <div class="col-2">
                                         <div style="position: relative; width: 100%; padding-bottom: 100%;">
                                             <img src="${item.img}" class="img-fluid rounded position-absolute top-0 start-0 w-100 h-100" style="object-fit: cover;" alt="${item.description}">
                                         </div>
                                     </div>
                                     `}
-                                    <div class=" ${item.img == null ? 'col-12 col-sm-8 col-md-6 col-lg-6' : 'col-10 col-sm-4 col-md-4 col-lg-4'}">
+
+                                    <div class=" ${item.img == null ? 'col-8' : 'col-6'}">
                                         <h6 class="card-title mb-1 fw-bold text-truncate">${item.description}</h6>
                                         <p class="card-text text-muted small mb-1">${item.description}</p>
                                         <span class="badge bg-primary text-white">ID: ${item.id}</span>
                                         <span class="badge bg-secondary text-white">${item.brand}</span>
-
                                     </div>
-                                    <div class="col-md-2 col-4">
+                                    
+                                    <div class="col-2">
                                         <div class="input-group input-group-sm">
                                             <input type="number" class="form-control text-center quantity-input" 
                                                 value="${item.quantity}" min="1" 
@@ -404,7 +406,7 @@ startTemplate("Inventarios");
                                         </div>
                                     </div>
                                     
-                                    <div class="col-md-2 col-4 text-end">
+                                    <div class="col-2 text-end">
                                         <button class="btn btn-outline-danger btn-sm" 
                                                 onclick="removeFromCart('${item.id}')"
                                                 aria-label="Eliminar toolo"
